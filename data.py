fffb63f0daee79ea0c3738b1d789491b2803bb7b
Viktoriya Merkylova <v.valkiriya1@yandex.ru>
2025-03-22 17:17:35 +0300

    generate_email

--- data.py
+++ data.py
@@ -1,9 +1,35 @@
 import random
+import string
 
-def generate_email (number, domain):
-    name = "viktoriya"
-    surname = "merkylova"
-    random_digits = random.randint(100, 999)
-    email = f"{surname}_{name}_{number}_{random_digits}@{domain.lower()}"
-    return email
+class AuthGenerator:
+    def generate_email (self, number, domain):
+        name = "viktoriya"
+        surname = "merkylova"
+        number = 17
+        random_digits = random.randint(100, 999)
+        email = f"{surname}_{name}_{number}_{random_digits}@{domain.lower()}"
+        return email
+
+    def generate_password(self, length=6):
+        if length < 6:
+            raise ValueError("Минимальная длина пароля - 6 символов")
+
+        lowercase = string.ascii_lowercase
+        uppercase = string.ascii_uppercase
+        digits = string.digits
+        special = string.punctuation
+
+        password = [
+            random.choice(lowercase),
+            random.choice(uppercase),
+            random.choice(digits),
+            random.choice(special)
+        ]
+
+        all_chars = lowercase + uppercase + digits + special
+        password += random.choices(all_chars, k=length - 4)
+
+        random.shuffle(password)
+
+        return ''.join(password)
 
