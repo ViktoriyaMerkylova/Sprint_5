270b37a5220ac4caefee8ed92a56128ced9ab818
Viktoriya Merkylova <v.valkiriya1@yandex.ru>
2025-03-23 13:19:40 +0300

    create test_login_via_profile_button

--- curl.py
+++ curl.py
@@ -1,3 +1,4 @@
 
 main_site = 'https://stellarburgers.nomoreparties.site/'
 main_login = 'https://stellarburgers.nomoreparties.site/login'
+main_register = 'https://stellarburgers.nomoreparties.site/register'
--- tests/test_enty.py
+++ tests/test_enty.py
@@ -30,3 +30,24 @@ class TestLoginService:
         )
 
         assert driver.current_url == main_site
+
+    def test_login_via_profile_button(self, driver):
+        driver.get(main_site)
+        WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(Locators.PROFILE_LINK)
+        )
+        driver.find_element(*Locators.PROFILE_LINK).click()
+
+        email = auth_generator.email_register
+        password = auth_generator.password_register
+
+        driver.find_element(*Locators.NAME).send_keys(email)
+        driver.find_element(*Locators.PASSWORD_INPUT).send_keys(password)
+
+        driver.find_element(*Locators.BTN_ENTER).click()
+
+        WebDriverWait(driver, 10).until(
+            EC.url_to_be(main_site)
+        )
+
+        assert driver.current_url == main_site
